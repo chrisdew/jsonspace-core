4c563bb0de5b012959d703b3e1630000d773a7d2
chrisdew <cmsdew@gmail.com>
2013-05-20 13:15:45 +0100

    added constructor test

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-jsc
-===
+Installation
+============
 
-jsc
+Run `npm install` in the root of the repo to fetch the dependencies.
+
+To make code coverage work, you'll need to `cd` into `vendor/node-jscoverage` and `configure && make && sudo make install`.
+
--- test/jsonspace-test.js
+++ test/jsonspace-test.js
@@ -1,9 +1,13 @@
 var assert = require("assert")
 
-var jsonspace = require('../lib/index.js').jsonspace;
+var js = require('../lib/index.js').jsonspace;
 describe('The example module', function() {
   it('should say "this function is tested"', function() {
-    assert.equal("this function is tested", jsonspace.tested());
+    assert.equal('this function is tested', js.tested());
+  });
+  it('should create a JsonSpace', function() {
+    var space = new js.JsonSpace();
+    assert('JsonSpace', space.constructor.name); 
   });
 });
 
