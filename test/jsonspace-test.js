fb1578e86493adc4523ab231f208554abf973c44
chrisdew <cmsdew@gmail.com>
2013-05-21 13:03:30 +0100

    added take functionality

--- test/jsonspace-test.js
+++ test/jsonspace-test.js
@@ -21,14 +21,50 @@ describe('The example module', function() {
     assert.deepEqual([{"hello": "world"}], space.read(function(ob) {
       return ob.hello !== undefined; 
     }));
+    assert.deepEqual([{"hello": "world"},{"foo": "bar"}], space.read());
   });
-  it('should register a read function', function(done) {
+  it('should register a reading onPut function', function(done) {
     var space = new js.JsonSpace();
-    space.on('put', function(ob) {
-      assert.deepEqual([{"hello": "world"}], space.read());
-      done();
+    space.onPut(function(ob) {
+      assert.deepEqual({"hello": "world"}, ob);
+      assert.deepEqual([], space.read());
+      process.nextTick(function() {
+        assert.deepEqual([{"hello": "world"}], space.read());
+        done();
+      });
+      return false; // don't take
     }); 
     space.put({"hello": "world"});
+  });
+  it('should register a taking onPut function', function(done) {
+    var space = new js.JsonSpace();
+    space.onPut(function(ob) {
+      assert.deepEqual({"hello": "world"}, ob);
+      assert.deepEqual([], space.read());
+      process.nextTick(function() {
+        assert.deepEqual([], space.read());
+        done();
+      });
+      return true; // take the object
+    }); 
+    space.put({"hello": "world"});
+  });
+  it('should allow a global take', function() {
+    var space = new js.JsonSpace();
+    space.put({"hello": "world"});
+    assert.deepEqual([{"hello": "world"}], space.read());
+    var taken = space.take();
+    assert.deepEqual([{"hello": "world"}], taken);
+    assert.deepEqual([], space.read());
+  });
+  it('should take with a function filter', function() {
+    var space = new js.JsonSpace();
+    space.put({"hello": "world"});
+    space.put({"foo": "bar"});
+    assert.deepEqual([{"hello": "world"}], space.take(function(ob) {
+      return ob.hello !== undefined; 
+    }));
+    assert.deepEqual([{"foo": "bar"}], space.read());
   });
 });
 
